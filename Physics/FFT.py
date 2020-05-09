86cbdee9e2634ecf758bded2d1e40c21d8a197b9
PyPhy <42840703+PyPhy@users.noreply.github.com>
2020-05-09 19:53:15 +0530

    Add files via upload

---
+++ Physics/FFT.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from numpy import pi, sin, append, linspace, array, arange
+from numpy.random import rand, seed
+from scipy.fftpack import fft
+import matplotlib.pyplot as plt
+from mpldatacursor import datacursor
+
+#%% FFT
+
+class FFT():
+    
+    def __init__(self, t, Signal):
+        
+        self.t = t
+        self.f = Signal
+        self.N = len(t)
+        
+    def Freq(self, f, N):
+        
+        freqs = arange(1, N, 1)[:int(N/2) - 1]
+        Ampts = (2/N)* abs( fft(f)[:int(N/2)] )[1:]
+        
+        return freqs, Ampts
+
+    def PlotFFT(self):
+        
+        fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(80, 60))
+        
+        # signal plot
+        axs[0].plot(self.t, self.f)
+        axs[0].grid()
+        axs[0].set_xlabel('time (s)', fontsize = 16)
+        axs[0].set_ylabel('Voltage', fontsize = 16)
+        axs[0].set_title('Signal', fontweight = 'bold', fontsize = 20)
+        
+        # frequency
+        freqs, Ampts = self.Freq( self.f, self.N)
+        
+        h = axs[1].plot(freqs, Ampts, 'r')
+        datacursor(h)
+        axs[1].grid()
+        axs[1].set_xlabel('frequency (Hz)', fontsize = 16)
+        axs[1].set_ylabel('Voltage', fontsize = 16)
+        axs[1].set_title('FFT', fontweight = 'bold', fontsize = 20)
+        
+        plt.show()
+
+#%%
+
+if __name__ == '__main__':
+    
+    # time
+    t = linspace(0, 1, 400)
+    
+    # Example - 1: Signal
+    # f = sin(2* pi* 5* t) + 0.5* sin(2* pi* 10* t)
+    
+    # Example - 2: Square wave
+    # f = array([])
+    # for i in t:
+    #     if i <= 0.5:
+    #         f = append(f, 1)
+    #     else:
+    #         f = append(f, -1)
+    
+    # Example - 3: Signal with noise
+    seed(1)
+    f = sin(2* pi* 5* t) + 0.5* sin(2* pi* 10* t) + rand( len(t) )
+    
+    SignalFFT = FFT(t, f)
+    SignalFFT.PlotFFT()
---
+++ Physics/FS_square.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+from matplotlib.patches import ConnectionPatch
+
+#%% Class for Fourier Series
+
+class FS():
+    
+    def __init__(self, Circles, Cycles):
+        
+        self.Circles = Circles
+        self.Cycles  = Cycles
+        
+    #%% X coordinate of circle's center
+    
+    def Xcenter(self, n, theta):
+        
+        ''' 
+            X coordinate of n th circle
+        '''
+        
+        Ans = 0
+        
+        if n > 0:
+            for i in range(1, n + 1):
+                Ans += (4/( (2*i - 1)* np.pi))* np.cos( (2*i - 1)* theta)
+    
+        return Ans
+    
+    #%% Y coordinate of circle's center
+    
+    def Ycenter(self, n, theta):
+        
+        ''' 
+            Y coordinate of n th circle
+        '''
+        
+        Ans = 0
+        
+        if n > 0:
+            for i in range(1, n + 1):
+                Ans += (4/( (2*i - 1)* np.pi))* np.sin( (2*i - 1)* theta)
+    
+        return Ans
+    
+    #%% Radius of circle
+    
+    def Rds(self, n):
+        
+        ''' 
+            Radius of n th circle
+        '''
+        
+        return 4/( (2*n + 1)* np.pi)
+    
+    #%% Plot
+    
+    def PlotFS(self):
+
+        time = np.linspace(0, self.Cycles, self.Cycles* 70)
+        
+        fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(80, 60))
+        fig.suptitle('Fourier Series', fontsize = 45, fontweight = 'bold')
+        
+        color = cm.rainbow( np.linspace(0, 1, self.Circles) )
+        
+        for t in time:
+            
+            thta = 2* np.pi* t
+            
+            #%% clear the plot
+            axs[0].clear()
+            
+            if (t > 0):
+                con.remove()
+            
+            #%% First plot
+            
+            for i, c in zip(range(0, self.Circles), color):
+                xc = self.Xcenter(i, thta)
+                yc = self.Ycenter(i, thta)
+                R  = self.Rds(i)
+                
+                crl = plt.Circle((xc, yc), R, color=c, alpha = 0.5, linewidth = 2)
+                axs[0].add_artist(crl)
+                
+                if (i > 0):
+                    axs[0].plot([xco, xc], [yco, yc], color='b', linewidth = 2)
+                
+                xco = xc
+                yco = yc
+                Ro  = R
+            
+            axs[0].axis('square')
+            axs[0].set_xlim([ -9/np.pi, 9/np.pi ])
+            axs[0].set_ylim([ -9/np.pi, 9/np.pi ])
+            
+            #%% Second plot
+            
+            if (t > 0):
+                axs[1].plot([to,t], [ycirc, yc], color='m', linewidth = 1.5)
+            
+            to    = t
+            ycirc = yc
+            
+            axs[1].axis('square')
+            axs[1].set_xlim([ 0, 18/np.pi ])
+            axs[1].set_ylim([ -9/np.pi, 9/np.pi ])
+        
+            #%% Line
+            
+            con = ConnectionPatch( xyA = (t,  yc), xyB = (xc, yc), 
+                                   coordsA = 'data', coordsB = 'data',
+                                   axesA = axs[1], axesB = axs[0], 
+                                   color = 'red')
+            axs[1].add_artist(con)
+            
+            plt.pause(1e-11)
+
+#%% Main
+
+if __name__ == '__main__':
+    
+    # Circles, Cycles
+    fs = FS(8, 2)
+    fs.PlotFS()
