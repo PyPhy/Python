61a7881c3bf9a7f5112457859fab3d7659e04136
PyPhy <42840703+PyPhy@users.noreply.github.com>
2020-06-02 21:27:03 +0530

    Update Animation.py

--- Fun codes/Animation.py
+++ Fun codes/Animation.py
@@ -5,41 +5,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-#%% Data
+#%%
 
-x = np.linspace(0, 2*np.pi, 200)
-y = np.sin(x)
-
-#%% Animate
-
-x_Frame, y_Frame = [], []
-
-fig, ax = plt.subplots()
-ln, = plt.plot([], [], linewidth = 2)
-
-def PlotSettings():
+class AnimateData:
     
-    ax.set_title('Sine wave', fontsize = 20, fontweight = 'bold')
-    
-    ax.set_xlabel('x', fontsize = 16, fontweight = 'bold')
-    ax.set_ylabel('y', fontsize = 16, fontweight = 'bold')
-    
-    ax.set_xlim(0, 2*np.pi)
-    ax.set_ylim(-1, 1)
-    
-    return ln,
+    def __init__(self, x, y):
+        
+        self.x = x
+        self.y = y
 
-def NewFrame(frame):
+        self.x_Frame, self.y_Frame = [], []
+        
+        self.fig, self.ax = plt.subplots()
+        self.ln, = plt.plot([], [], linewidth = 2)
     
-    x_Frame.append(x[frame])
-    y_Frame.append(y[frame])
+    def PlotSettings(self):
+        
+        self.ax.set_title('Sine wave', fontsize = 20, fontweight = 'bold')
+        
+        self.ax.set_xlabel('x', fontsize = 16, fontweight = 'bold')
+        self.ax.set_ylabel('y', fontsize = 16, fontweight = 'bold')
+        
+        self.ax.set_xlim(0, 2*np.pi)
+        self.ax.set_ylim(-2, 2)
+        
+        return self.ln,
     
-    ln.set_data(x_Frame, y_Frame)
-    
-    return ln,
+    def NewFrame(self, frame):
+        
+        self.x_Frame.append( self.x[frame] )
+        self.y_Frame.append( self.y[frame] )
+        
+        self.ln.set_data(self.x_Frame, self.y_Frame)
+        
+        return self.ln,
 
-ani = FuncAnimation(fig, NewFrame, frames = range(0, len(x)),
-                    init_func = PlotSettings, blit=True)
+    def RunTheCode(self):
+        
+        Animation = FuncAnimation(self.fig, self.NewFrame, frames = range(0, len(self.x)),
+                                  init_func = self.PlotSettings, blit = True)
+        
+        Animation.save('SineWave.mp4', writer = 'ffmpeg', fps = 20)
+        plt.show()
 
-ani.save('SineWave.mp4', writer = 'ffmpeg', fps = 20)
-plt.show()
+#%%
+
+if __name__ == '__main__':
+    
+    x = np.linspace(0, 2*np.pi, 200)
+    y = np.sin(x)
+    
+    DoMyWork = AnimateData(x, y)
+    DoMyWork.RunTheCode()
