d091a6e5b1021d37ff129c094a66704dd185c909
PyPhy <42840703+PyPhy@users.noreply.github.com>
2020-07-16 18:02:48 +0530

    Add files via upload

---
+++ Molecular Dynamics/Multi_Particles/LJ/Main.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void Load(int Np, double *M, double *x, double *y, double *Vx, double *Vy)
+{
+	FILE * f1 = fopen("Data/x.txt",  "r");	// Load x coordinates
+	FILE * f2 = fopen("Data/y.txt",  "r");	// Load y coordinates
+	FILE * f3 = fopen("Data/vx.txt", "r");	// Load Vx Velocity
+	FILE * f4 = fopen("Data/vy.txt", "r");	// Load Vy Velocity
+	FILE * f5 = fopen("Data/M.txt",  "r");  // Load M mass
+
+	for(int i = 0; i < Np; i++){
+		fscanf(f1, "%lE\t", &x[i]);
+		fscanf(f2, "%lE\t", &y[i]);
+		fscanf(f3, "%lE\t", &Vx[i]);
+		fscanf(f4, "%lE\t", &Vy[i]);
+		fscanf(f5, "%lE\t", &M[i]);
+	}
+
+	fclose(f1);
+	fclose(f2);
+	fclose(f3);
+	fclose(f4);
+	fclose(f5);
+}
+
+void Acc(int Np, double rm, double ep, double *M, double *x, double *y, double *Ax, double *Ay)
+{
+    float r, C;
+
+    for(int i = 0; i < Np; i++){
+
+        Ax[i] = 0.0;
+        Ay[i] = 0.0;
+
+        for(int j = 0; j < Np; j++){
+            if(i != j){
+
+                r = sqrt( pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2) );
+                C = (12* ep/ pow(rm, 2))* ( pow(rm/ r, 14) - pow(rm/ r, 8) )/ M[i];
+
+                Ax[i] += C* (x[i] - x[j]);
+                Ay[i] += C* (y[i] - y[j]);
+            }
+        }
+    }
+}
+
+void main()
+{
+	double Data[5];
+	FILE * fImpVar = fopen("Inputs.txt", "r");
+	for(int i = 0; i < 5; i++){
+		fscanf(fImpVar, "%*s %*s %lf\n", &Data[i]);
+		printf("%f\n", Data[i]);
+	}
+	fclose(fImpVar);
+
+	double tmax = Data[1], dt = Data[2], rm = Data[3], eps = Data[4], t = 0;
+	int N = Data[0];
+
+	double M[N], x[N], y[N], vx[N], vy[N], aox[N], aoy[N], a1x[N], a1y[N], a2x[N], a2y[N];
+
+	Load(N, M, x, y, vx, vy);
+
+	FILE * fX  = fopen("Data/x.txt",  "w");
+	FILE * fY  = fopen("Data/y.txt",  "w");
+	FILE * fVx = fopen("Data/vx.txt", "w");
+	FILE * fVy = fopen("Data/vy.txt", "w");
+
+	// Euler Cromer method
+	Acc(N, rm, eps, M, x, y, aox, aoy);
+
+	for(int i = 0; i < N; i++){
+		vx[i] += aox[i]* dt;
+		vy[i] += aoy[i]* dt;
+
+		x[i]  += vx[i]* dt;
+		y[i]  += vy[i]* dt;
+	}
+
+	t += dt;
+
+	Acc(N, rm, eps, M, x, y, a1x, a1y);
+	for(int i = 0; i < N; i++){
+		fprintf(fX,  "%lE\t", x[i]);
+		fprintf(fY,  "%lE\t", y[i]);
+		fprintf(fVx, "%lE\t", vx[i]);
+		fprintf(fVy, "%lE\t", vy[i]);
+	}
+	fprintf(fX,  "\n");
+	fprintf(fY,  "\n");
+	fprintf(fVx, "\n");
+	fprintf(fVy, "\n");
+
+	while(t <= tmax){
+
+		for(int i = 0; i < N; i++){
+			x[i] += vx[i]* dt + (4* a1x[i] - aox[i])* pow(dt, 2)/ 6;
+			y[i] += vy[i]* dt + (4* a1y[i] - aoy[i])* pow(dt, 2)/ 6;
+		}
+
+		Acc(N, rm, eps, M, x, y, a2x, a2y);
+
+		for(int i = 0; i < N; i++){
+			vx[i] += (5* a2x[i] + 8* a1x[i] - aox[i])* dt/ 12;
+			vy[i] += (5* a2y[i] + 8* a1y[i] - aoy[i])* dt/ 12;
+		}
+
+		// Save data
+		for(int i = 0; i < N; i++){
+			fprintf(fX,  "%lE\t", x[i]);
+			fprintf(fY,  "%lE\t", y[i]);
+			fprintf(fVx, "%lE\t", vx[i]);
+			fprintf(fVy, "%lE\t", vy[i]);
+		}
+		fprintf(fX,  "\n");
+		fprintf(fY,  "\n");
+		fprintf(fVx, "\n");
+		fprintf(fVy, "\n");
+
+		for(int i = 0; i < N; i++){
+			aox[i] = a1x[i];
+			aoy[i] = a1y[i];
+		}
+
+		for(int i = 0; i < N; i++){
+			a1x[i] = a2x[i];
+			a1y[i] = a2y[i];
+		}
+
+		// time update
+		t += dt;
+	}
+
+	fclose(fX);
+	fclose(fY);
+	fclose(fVx);
+	fclose(fVy);
+
+	printf("Hey Divyang!!! Code run completed successfully !!!\n");
+	printf("%f\n", t);
+}
---
+++ Molecular Dynamics/Multi_Particles/LJ/PlotDataForC.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 11 17:01:44 2020
+
+@author: divyang
+"""
+
+from numpy import array, sum, append, delete, shape, sqrt, linspace
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+#%%
+
+class Energy:
+
+    '''
+        This class will calculate Kinetic, Potential, and Total Energy per particle.
+    '''
+
+    def __init__(self, rm, ep, M, N, x, y, Vx, Vy):
+
+        self.rm, self.ep = rm, ep
+        self.M,  self.N  = M,  N
+        self.x,  self.y  = x,  y
+        self.Vx, self.Vy = Vx, Vy
+
+
+    def Pot(self, x, y):
+
+        W = 0
+
+        for i in range(0, self.N):
+            for j in range(0, self.N):
+                if (i != j):
+
+                    r = ( (x[i] - x[j])**2 + (y[i] - y[j])**2 )**0.5
+
+                    W += 0.5* self.ep* ( (self.rm/ r)**12 - 2* (self.rm/ r)**6 )
+
+        return W
+
+
+    def EnergyCalculations(self):
+
+        # kinetic energy
+        KE = 0
+        for i in range(0, len(self.M)):
+            KE += 0.5* self.M[i]* ( self.Vx[:,i]**2 + self.Vy[:,i]**2 )
+
+        # KE = 0.5* sum( self.Vx**2 + self.Vy**2, axis=1)
+
+        # electrostatic potential energy
+        p, _ = shape( self.x )
+        PE = array([])
+        for i in range(0, p):
+            PE_dt = self.Pot( self.x[i,:], self.y[i,:] )
+            PE = append(PE, PE_dt)
+
+        # Total energy
+        E = KE + PE
+
+        # Energy per particle
+        E  = E / self.N
+        KE = KE/ self.N
+        PE = PE/ self.N
+
+        return E, KE, PE
+
+#%%
+
+class PlotData:
+
+    def __init__(self):
+
+        # Read Data Files
+        M    = pd.read_csv(r'Data/M.txt', header = None, sep = ' ')
+        x    = pd.read_csv(r'Data/x.txt',  header = None, sep = '\t')
+        y    = pd.read_csv(r'Data/y.txt',  header = None, sep = '\t')
+        Vx   = pd.read_csv(r'Data/vx.txt', header = None, sep = '\t')
+        Vy   = pd.read_csv(r'Data/vy.txt', header = None, sep = '\t')
+        data = pd.read_csv('Inputs.txt', header = None, sep = '=')
+        data = data[1].tolist()
+        
+        self.N  = int(data[0])
+        
+        # Delet the last column
+        x  =  x.drop(self.N, 1)
+        y  =  y.drop(self.N, 1)
+        Vx = Vx.drop(self.N, 1)
+        Vy = Vy.drop(self.N, 1)
+
+        self.M  = M.values.tolist()[0]
+        self.x  = array(x)
+        self.y  = array(y)
+        self.Vx = array(Vx)
+        self.Vy = array(Vy)
+        self.rm = float(data[3])
+        self.ep = float(data[4])
+
+        p, _ = shape(x)
+        self.time = linspace(0, (p-1)* data[2], p)
+
+        self.E, self.KE, self.PE = Energy(self.rm, self.ep, self.M, self.N, self.x, self.y, self.Vx, self.Vy).EnergyCalculations()
+
+    def AnimamteParticles(self, Skip = 20):
+
+        gs = GridSpec(1, 2)
+
+        fig = plt.figure( figsize = (80, 60) )
+        ax1 = fig.add_subplot( gs[0, 0] )
+        ax2 = fig.add_subplot( gs[0, 1] )
+
+        for frames, t in enumerate( self.time[::Skip] ):
+
+            fS = frames* Skip
+
+            # PLOT - 1: Particles
+            ax1.clear()
+
+            ax1.scatter( self.x[fS], self.y[fS], marker = 'o' )
+
+            Vmag = ( self.Vx[fS]**2 + self.Vy[fS]**2 )**0.5
+
+            ax1.quiver(  self.x[fS],  self.y[fS], \
+                        self.Vx[fS], self.Vy[fS], \
+                        Vmag, width = 0.003 )
+
+            ax1.set_xlabel('x', fontsize = 16)
+            ax1.set_ylabel('y', fontsize = 16)
+            ax1.set_xlim([-5, 5])
+            ax1.set_ylim([-5, 5])
+            ax1.set_title('FRA', fontweight = 'bold', fontsize = 20)
+
+            # PLOT - 2: Energy
+            if (frames == 0):
+
+                e  = self.E[fS]
+                ke = self.KE[fS]
+                pe = self.PE[fS]
+                to = t
+
+                ax2.scatter(to,  e, label = r'$E_{sys}$')
+                ax2.scatter(to, ke, label = r'KE')
+                ax2.scatter(to, pe, label = r'PE')
+
+                ax2.legend()
+
+            else:
+
+                ax2.plot([to,t], [ e, self.E[fS]  ], color = 'blue')
+                ax2.plot([to,t], [ke, self.KE[fS] ], color = 'orange')
+                ax2.plot([to,t], [pe, self.PE[fS] ], color = 'green')
+
+                e  = self.E[fS]
+                ke = self.KE[fS]
+                pe = self.PE[fS]
+                to = t
+
+            plt.pause(1e-11)
+
+
+    def PlotEnergy(self):
+
+        fig = plt.figure('Energy conservation')
+        plt.plot( self.time, self.E,  label = r'$E_{sys}$')
+        plt.plot( self.time, self.KE, label = r'KE')
+        plt.plot( self.time, self.PE, label = r'PE')
+        plt.legend()
+        plt.grid()
+        plt.show()
+
+
+#%%
+
+if __name__ == '__main__':
+
+    DoMyWork = PlotData()
+    # DoMyWork.AnimamteParticles(Skip = 200)
+    DoMyWork.PlotEnergy()
