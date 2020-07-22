21e5532a2a8392ba0bdd47dfda3198d51ca9b0ef
PyPhy <42840703+PyPhy@users.noreply.github.com>
2020-07-22 14:29:12 +0530

    Update Initial.py

--- Molecular Dynamics/Multi_Particles/Coulomb/Initial.py
+++ Molecular Dynamics/Multi_Particles/Coulomb/Initial.py
@@ -11,9 +11,20 @@ class MakeDataFiles:
     
     def __init__(self):
         
-        self.x,  self.y  = [-1, 1], [0, 0]  # Position
-        self.Vx, self.Vy = [0, 0], [-0.5, 0.5]  # Velocity
-        self.Q = [1, -1] # Charge
+        # Experiment: 1
+        # self.x,  self.y  = [-0.5, 0.5, -0.5, 0.5], [-0.5, -0.5, 0.5, 0.5]
+        # self.Vx, self.Vy = [0, 0, 0, 0], [0, 0, 0, 0]
+        # self.Q = [1, 1, 1, 1]
+        
+        # Experiment: 2
+        # self.x,  self.y  = [-1, 1], [0, 0]
+        # self.Vx, self.Vy = [0, 0], [-0.5, 0.5]
+        # self.Q = [1, -1]
+        
+        # Experiment: 3
+        self.x,  self.y  = [-1, 1, -1, 1], [-1, -1, 1, 1]
+        self.Vx, self.Vy = [-0.4, 0, 0, 0.4], [0, -0.4, 0.4, 0]
+        self.Q = [-1, 1, 1, -1]
     
 
     def WriteDataFiles(self):
